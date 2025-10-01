ab342e976363a56b0af19e332762c644e85f515b
KUANG-JIAN-AI <k025c1007@g.neec.ac.jp>
2025-10-01 09:27:34 +0900

    消息列表调整 v0.11

--- app/controllers/msg.py
+++ app/controllers/msg.py
@@ -1,6 +1,8 @@
 from flask import current_app, request
+from sqlalchemy import and_, or_
 
 from app.models.msg import Msg, db
+
 
 def send_msg():
     data = request.get_json()  # 从请求体里解析 JSON
@@ -10,7 +12,7 @@ def send_msg():
 
     with current_app.app_context():
         if not sender_id or not receiver_id or not message:
-            return {'code': 400, 'msg': 'missing'}
+            return {"code": 400, "msg": "missing"}
 
         msg = Msg(sender_id=sender_id, receiver_id=receiver_id, message=message)
 
@@ -18,8 +20,16 @@ def send_msg():
             db.session.add(msg)
             db.session.commit()
 
-        return {'code': 200, 'msg': 'success'}
-    
+        return {"code": 200, "msg": "success"}
+
+
 def get_msg():
-    msgs = Msg.query.all()
+    sender_id = request.args.get("sender_id")
+    receiver_id = request.args.get("receiver_id")
+    msgs = Msg.query.filter(
+        or_(
+            and_(Msg.sender_id == sender_id, Msg.receiver_id == receiver_id),
+            and_(Msg.sender_id == receiver_id, Msg.receiver_id == sender_id),
+        )
+    ).all()
     return {"code": 200, "msg": "success", "data": [m.to_dict() for m in msgs]}
